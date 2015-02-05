519c93c7ce70d1ad3b73d47019071d3b05b92ceb
vijaybhat79 <vijaybhat79@gmail.com>
2015-02-05 15:40:09 -0500

    Update myblog.js

--- js/myblog.js
+++ js/myblog.js
@@ -1,3 +1,14 @@
+function getMyCVFeed(value) {
+  alert("cv");
+  return "http://google.com";
+}
+
 function getMyBlogFeed(value) {
-  alert("value : " + value);
+  alert("blog");
+  return "http://google.com";
+}
+
+function getMyHomeFeed(value) {
+  alert("home");
+  return "http://google.com";
 }
